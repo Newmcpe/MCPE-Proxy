9375e35d551632a1fe85684228f0299eced29012
Newmcpe <chebakov05@gmail.com>
2017-10-31 12:10:02 +0800

    title

--- plugins/Fly/Frago9876543210/Fly/FlyPlugin.php
+++ plugins/Fly/Frago9876543210/Fly/FlyPlugin.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Frago9876543210\Fly;
 
 
-use pocketmine\network\mcpe\protocol\AdventureSettingsPacket;
+use pocketmine\network\mcpe\protocol\SetTitlePacket;
 use pocketmine\network\mcpe\protocol\DataPacket;
 use proxy\plugin\ProxyPluginBase;
 use proxy\Proxy;
@@ -23,16 +23,17 @@ class FlyPlugin extends ProxyPluginBase
 
     public function onDataPacketFromServer(DataPacket $packet): bool
     {
-        if ($packet instanceof AdventureSettingsPacket) {
-            $send = new AdventureSettingsPacket;
-            $send->flags = 615;
-            $send->commandPermission = 0;
-            $send->flags2 = 31;
-            $send->playerPermission = 1;
-            $send->customFlags = 0;
-            $send->entityUniqueId = $packet->entityUniqueId;
-            $this->getProxy()->writeDataPacket($send, $this->getProxy()->clientHost, $this->getProxy()->clientPort);
-        }
+        	$pk = new SetTitlePacket();
+			$pk->type = SetTitlePacket::TITLE_TYPE_TIMES;
+			$pk->text = "";
+			$pk->fadeInTime = 5;
+			$pk->fadeOutTime = 5;
+			$pk->stayTime = 20 * $time;
+         $this->getProxy()->writeDataPacket($pk, $this->getProxy()->clientHost, $this->getProxy()->clientPort);
+        $pk = new SetTitlePacket();
+			$pk->type = SetTitlePacket::TITLE_TYPE_TITLE;
+			$pk->text = "Привет";
+         $this->getProxy()->writeDataPacket($pk, $this->getProxy()->clientHost, $this->getProxy()->clientPort);
         return true;
     }
 
